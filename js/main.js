53af6fa29f47de668b9125eb912227a7b6453003
Jrivera <jump31@hotmail.com>
2020-04-17 00:31:22 -0700

    js test

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//var contra = prompt("Ingresa tu contrasena");
+//console.log(contra);
+
+function changeColor(newColor){
+    var elem = document.getElementById('elemento');
+    elem.style.color=newColor;
+
+}
